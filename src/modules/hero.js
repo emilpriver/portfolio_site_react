7c79be78daf873f43f19a13ed6031d2e3e677022
Emil Priver <hello@emilpriver.com>
2019-04-01 23:44:17 +0200

    Update hero.js

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -8,7 +8,7 @@ export default class Menu extends React.Component{
                     <div className="text"> 
                        <div className="wrapper">
                             <h1>
-                                I'm Emil, An developer from Borås,Sweden.
+                                I'm Emil, A developer from Borås,Sweden.
                             </h1>
                        </div>
                     </div>  
