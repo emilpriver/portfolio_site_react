15c5c82b37f35f8c4981accbb4d4ba63d84c30a0
emilpriver <hello@emilpriver.com>
2019-04-02 23:16:47 +0200

    initä

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './components/about'
 import Single_project from './components/single_work'
 import error from './components/error'
 import Menu from './modules/menu'
-import { play, exit } from './timelines'
+
 //react transitions
 import { Transition, TransitionGroup } from 'react-transition-group'
 import ReactGA from 'react-ga'
@@ -22,16 +22,10 @@ class App extends Component {
         <div className="app">
           <Menu white_background={true} />
           <Route render={({ location }) => {
-          const { pathname, key } = location;
             return (
                 <TransitionGroup component={null}>
-                  <Transition
-                    key={key}
-                    appear={true}
-                    onEnter={(node, appears) => play(pathname, node, appears)}
-                    onExit={(node, appears) => exit(node, appears)}
-                    timeout={{enter: 750, exit: 150}}
-                  >
+                  <Transition 
+                  timeout={false}>
                     <Switch location={location}>
                       <Route exact path="/" component={Home}  />
                       <Route exact path="/missions" component={Missions}  />
